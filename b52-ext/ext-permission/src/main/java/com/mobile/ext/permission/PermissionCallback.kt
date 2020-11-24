8a557f7317ec203a5fac579c8a690013128c4c56
Jordan <Jordan>
2020-11-24 14:11:55 +0800

    权限修改

--- b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionCallback.kt
+++ b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionCallback.kt
@@ -2,6 +2,6 @@ package com.mobile.ext.permission
 
 interface PermissionCallback {
     fun passPermissions()
-    fun onRequestPermissions(permissions: Array<String>,
-                             grantResults: IntArray)
+//    fun onRequestPermissions(permissions: Array<String>,
+//                             grantResults: IntArray)
 }
--- b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionRequest.kt
+++ b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionRequest.kt
@@ -8,13 +8,20 @@ import android.os.Build
 import android.provider.Settings
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDialogFragment
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 
 object PermissionRequest {
 
     private var callback: PermissionCallback? = null
 
-    fun request(activity: Activity, permission: Array<String>, cb: PermissionCallback) {
+    fun request(
+        activity: Activity,
+        requestCode: Int,
+        permission: Array<String>,
+        cb: PermissionCallback
+    ) {
         callback = cb
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  //>=6.0
             var granted = 0
@@ -28,13 +35,41 @@ object PermissionRequest {
             } else {
                 activity.requestPermissions(
                     permission,
-                    1
+                    requestCode
                 )
             }
         } else {
             callback?.passPermissions()
         }
     }
+
+    fun request(
+        fragment: Fragment,
+        requestCode: Int,
+        permission: Array<String>,
+        cb: PermissionCallback
+    ) {
+        callback = cb
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  //>=6.0
+            var granted = 0
+            for (i in permission) {
+                granted += ContextCompat.checkSelfPermission(fragment.requireActivity(), i)
+            }
+            if (granted
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                callback?.passPermissions()
+            } else {
+                fragment.requestPermissions(
+                    permission,
+                    requestCode
+                )
+            }
+        } else {
+            callback?.passPermissions()
+        }
+    }
+
     fun alertPermission(@StringRes msgRes: Int, activity: Activity?) {
         activity?.let {
             AlertDialog.Builder(it)
@@ -44,7 +79,7 @@ object PermissionRequest {
                     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                     val uri = Uri.fromParts("package", it.packageName, null)
                     intent.data = uri
-                    activity.startActivity(intent)
+                    it.startActivity(intent)
                     dialog.dismiss()
                 }
                 .create()
