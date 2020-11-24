c8d4ae76be62d3bc5af43c587ce1ff7b08db91da
Jordan <Jordan>
2020-11-24 11:33:50 +0800

    修改权限

--- b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionRequest.kt
+++ b52-ext/ext-permission/src/main/java/com/mobile/ext/permission/PermissionRequest.kt
@@ -2,39 +2,45 @@ package com.mobile.ext.permission
 
 import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
 import android.provider.Settings
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.FragmentManager
+import androidx.core.content.ContextCompat
 
 object PermissionRequest {
-    private const val TAG = "InvisibleFragment"
-    fun request(
-        fragmentManager: FragmentManager,
-        permission: Array<String>,
-        callback: PermissionCallback
-    ) {
-        val existedFragment = fragmentManager.findFragmentByTag(TAG)
-        val fragment = if (existedFragment != null) {
-            existedFragment as InvisibleFragment
-        } else {
-            val invisibleFragment = InvisibleFragment()
-            fragmentManager.beginTransaction().add(
-                invisibleFragment,
-                TAG
-            ).commitNow()
-            invisibleFragment
-        }
-        fragment.requestNow(callback, permission)
-    }
 
+    private var callback: PermissionCallback? = null
+
+    fun request(activity: Activity, permission: Array<String>, cb: PermissionCallback) {
+        callback = cb
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {  //>=6.0
+            var granted = 0
+            for (i in permission) {
+                granted += ContextCompat.checkSelfPermission(activity, i)
+            }
+            if (granted
+                == PackageManager.PERMISSION_GRANTED
+            ) {
+                callback?.passPermissions()
+            } else {
+                activity.requestPermissions(
+                    permission,
+                    1
+                )
+            }
+        } else {
+            callback?.passPermissions()
+        }
+    }
     fun alertPermission(@StringRes msgRes: Int, activity: Activity?) {
         activity?.let {
             AlertDialog.Builder(it)
-                .setTitle("温馨提示")
+                .setTitle(R.string.base_alert_title)
                 .setMessage(msgRes)
-                .setPositiveButton("确定") { dialog, which ->
+                .setPositiveButton(R.string.base_alert_known) { dialog, which ->
                     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
                     val uri = Uri.fromParts("package", it.packageName, null)
                     intent.data = uri
